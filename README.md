e95afb277123eee809a08f1809e36e23670cb05e
SilvioBi <biasiol.silvio@gmail.com>
2019-03-23 20:30:43 +0100

    Some small mods: center contact me form,  add email field validation, change site name and favicon

---
+++ README.md
@@ -0,0 +1,2 @@
+## TODO
+- center contact me form
--- src/components/contactMe/ContactMe.js
+++ src/components/contactMe/ContactMe.js
@@ -5,7 +5,7 @@ export default ({ namePlaceholder, emailPlaceholder, messagePlaceholder, submitB
     <div className="form-wrapper">
         <form id="form" class="topBefore" action="https://formspree.io/biasiol.silvio@gmail.com" method="POST">
             <input id="name" name="name" type="text" placeholder={namePlaceholder} />
-            <input id="email" name="email" type="text" placeholder={emailPlaceholder} />
+            <input id="email" name="email" type="email" placeholder={emailPlaceholder} />
             <textarea id="message" name="message" type="text" placeholder={messagePlaceholder} />
             <input id="submit" type="submit" value={submitButtonPlaceholder} />
         </form>
