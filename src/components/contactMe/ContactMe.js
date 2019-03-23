2d6bbc55cd361adcbf78ebe9d7b4dccdf8d11859
SilvioBi <biasiol.silvio@gmail.com>
2019-03-23 21:00:57 +0100

    Make all input in contact me required

--- src/components/contactMe/ContactMe.js
+++ src/components/contactMe/ContactMe.js
@@ -4,9 +4,9 @@ import './ContactMe.css'
 export default ({ namePlaceholder, emailPlaceholder, messagePlaceholder, submitButtonPlaceholder }) =>
     <div className="form-wrapper">
         <form id="form" class="topBefore" action="https://formspree.io/biasiol.silvio@gmail.com" method="POST">
-            <input id="name" name="name" type="text" placeholder={namePlaceholder} />
-            <input id="email" name="email" type="email" placeholder={emailPlaceholder} />
-            <textarea id="message" name="message" type="text" placeholder={messagePlaceholder} />
+            <input id="name" name="name" type="text" placeholder={namePlaceholder} required />
+            <input id="email" name="email" type="email" placeholder={emailPlaceholder} required />
+            <textarea id="message" name="message" type="text" placeholder={messagePlaceholder} required />
             <input id="submit" type="submit" value={submitButtonPlaceholder} />
         </form>
     </div>
