406c46cd41963186d0f60d1ae1389af0c64180ce
SilvioBi <biasiol.silvio@gmail.com>
2019-03-23 19:06:58 +0100

    Updates

---
+++ precache-manifest.eddf9bcc020fa62290bb120cb0e53a79.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "260d5299daa91c8a8a55",
+    "url": "/portfolio-site/static/css/main.aa0005fd.chunk.css"
+  },
+  {
+    "revision": "260d5299daa91c8a8a55",
+    "url": "/portfolio-site/static/js/main.4f03685f.chunk.js"
+  },
+  {
+    "revision": "ea85618c4c8be9d2492a",
+    "url": "/portfolio-site/static/js/runtime~main.7180bc28.js"
+  },
+  {
+    "revision": "7f4c9801b9e64f39df9f",
+    "url": "/portfolio-site/static/js/2.bc42ba1f.chunk.js"
+  },
+  {
+    "revision": "ed675667fed2b40c7c059e4f86bfb20e",
+    "url": "/portfolio-site/index.html"
+  }
+];
